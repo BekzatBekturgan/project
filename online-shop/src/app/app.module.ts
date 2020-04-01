b459745d5fe77978e5b7fdd9e04cb263d2280f80
Aigerim Tazhibay <aika0822t@gmail.com>
2020-04-01 18:59:44 +0600

    main, top-bar added

--- online-shop/src/app/app.module.ts
+++ online-shop/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
