cbd961d29a9a4fdaef4905280238203e8cec5f73
BekzatBekturgan <bbekturgan0@gmail.com>
2020-04-07 11:24:59 +0600

    update user.service.ts
    add method getUsers and getUsersByUsername

--- new-online-shop/src/app/user.service.ts
+++ new-online-shop/src/app/user.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { User } from './User';
+import { USERS } from './users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-constructor() { }
+  constructor() { }
 
+  getUsers(): Observable<User[]> {
+    return of(USERS);
+  }
+
+  getUser(username: String): Observable<User> {
+    return of(USERS.find(user => user.username === username));
+  }
+  
 }
