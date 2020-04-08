11e78b03688c70391f17fa2b34c881b5059efd3c
Yerassyl10 <daniyarbekuly.y@gmail.com>
2020-04-08 23:17:21 +0600

    one ngModel has been added

--- new-online-shop/src/app/app.module.ts
+++ new-online-shop/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CategoriesComponent } from './categories/categories.component';
@@ -31,6 +31,7 @@ import { ProductSearchComponent } from './product-search/product-search.componen
    ],
    imports: [
       BrowserModule,
+      FormsModule,
       AppRoutingModule,
       SliderModule,
       BrowserAnimationsModule,
