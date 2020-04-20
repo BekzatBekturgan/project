7731db6ae4c57b316ff1e1974c6d978deec6a716
Aigerim Tazhibay <aika0822t@gmail.com>
2020-04-20 18:00:48 +0600

    abort merge

---
+++ backend/main/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from main.models import Category, Product, Order, User
+
+
+class CategorySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    name = serializers.CharField()
+
+    def create(self, validated_data):
+        category = Category.objects.create(name=validated_data.get('name'))
+        return category
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.save()
+        return instance
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    # category = CategorySerializer2(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = Product
+        fields = '_all_'
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '_all_'
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField()
+    password = serializers.CharField()
+    firstName = serializers.CharField()
+    lastName = serializers.CharField()
+    address = serializers.CharField()
+    phone = serializers.CharField()
+
+    def create(self, validated_data):
+        user = User.objects.create(username=validated_data.get('username'),
+                                   password=validated_data.get('password'),
+                                   firstname=validated_data.get('firstName'),
+                                   lastname=validated_data.get('lastName'),
+                                   address=validated_data.get('address'),
+                                   phone=validated_data.get('phone'),
+                                   )
+
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.password = validated_data.get('password', instance.password)
+        instance.firstName = validated_data.get('firstName', instance.firstname)
+        instance.lastName = validated_data.get('lastName', instance.lastName)
+        instance.address = validated_data.get('address', instance.address)
+        instance.address = validated_data.get('phone', instance.phone)
+        instance.save()
+        return instance
---
+++ backend/main/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from main.models import Category, Product, Order, User
+from main.serializers import CategorySerializer, ProductSerializer, OrderSerializer, UserSerializer
+
+#CRUD and Serializer done
+@api_view(['GET', 'POST'])
+def category_list(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+#GET and SERIALIZER
+@api_view(['GET'])
+def products_by_category(request, category_id):
+    try:
+        categories = Category.objects.get(id=category_id)
+    except Category.DoesNotExist as e:
+        return Response({'error': str(e)})
+
+    if request.method == 'GET':
+        products = categories.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+#CRUD AND SERIALIZER DONE
+@api_view(['GET', 'POST'])
+def products_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+#CRUD AND SERIALIZER DONE
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, category_id):
+    try:
+        products = Product.objects.get(id=category_id)
+    except Product.DoesNotExist as e:
+        return Response({'error': str(e)})
+    if request.method == 'GET':
+        serializer = ProductSerializer(products)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(instance=products, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response({'error': serializer.errors})
+
+    elif request.method == 'DELETE':
+        products.delete()
+        return Response({'deleted': True})
+
+
+#order
+
+class OrdersListAPIView(APIView):
+    def get(self, request):
+        orders = Order.objects.all()
+        serializer = OrderSerializer(orders, many=True)
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors},
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+#user
+
+class UserAPIView(APIView):
+    def get_object(self, id):
+        try:
+            return User.objects.get(id=id)
+        except User.DoesNotExist as e:
+            return Response({'error': str(e)})
+
+    def get(self, request, id):
+        vacancy = self.get_object(id)
+        serializer = OrdersListAPIView(vacancy)
+        return Response(serializer.data)
