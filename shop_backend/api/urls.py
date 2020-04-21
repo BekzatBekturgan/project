72f0f178963e8c4e53296da6611984a8755cd1f2
Aigerim Tazhibay <aika0822t@gmail.com>
2020-04-21 20:21:08 +0600

    conflicts in urls file

--- shop_backend/api/urls.py
+++ shop_backend/api/urls.py
@@ -1,13 +1,6 @@
 from django.urls import path
-
-<<<<<<< HEAD
 from api.views import category_list, products_list, UserAPIView, product_detail, products_by_category, OrdersListAPIView, LoginView, LogoutView
-=======
-
 from api.views import category_list, products_list, UserAPIView, product_detail, products_by_category, OrdersListAPIView
->>>>>>> 4e2a46dbc7b515b2683c23d6a1c58bcaf59a1def
-
-
 
 urlpatterns = [
      path('categories/', category_list),
@@ -17,12 +10,10 @@ urlpatterns = [
 
      path('products/<int:product_id>/', product_detail),
      path('categories/<int:category_id>/products', products_by_category),
-<<<<<<< HEAD
+
      path('orders/', OrdersListAPIView.as_view()),
      path('login/', LoginView.as_view()),
      path('logout/', LogoutView.as_view()),
-=======
-     path('orders/', OrdersListAPIView.as_view())
 
->>>>>>> 4e2a46dbc7b515b2683c23d6a1c58bcaf59a1def
+
 ]
