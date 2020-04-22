7ed8801fc497cbb140d96a8438bdad25c3fcd15c
Yerassyl10 <daniyarbekuly.y@gmail.com>
2020-04-23 00:27:26 +0600

    new backend just dublicated totally connected with angular

---
+++ test/api/models.py
@@ -0,0 +1,89 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Company(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(default="")
+    city = models.CharField(max_length=200)
+    address = models.FloatField(default="")
+
+
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'city': self.city,
+            'address': self.address
+
+        }
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(default="")
+    salary = models.FloatField(default="")
+    company = models.ForeignKey(Company,  on_delete=models.CASCADE,
+                                related_name='vacancies')
+
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'salary': self.salary,
+            'company': self.company
+
+        }
+# --------------------------------------
+
+class Category(models.Model):
+    name = models.CharField(max_length=500)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    price = models.FloatField(default=0)
+    description = models.TextField()
+    image = models.CharField(max_length=1000)
+    sale = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'description': self.description,
+            'image': self.image,
+            'sale': self.sale
+        }
+
+
+class MyManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(username=user)
+
+
+class Order(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    items = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+    my_orders = MyManager()
+
+    def __str__(self):
+        return self.items.name
+
+    def to_json(self):
+        return{
+            'id': self.id,
+            'username': self.username,
+            'items': self.items
+        }
---
+++ test/api/views.py
@@ -0,0 +1,98 @@
+from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from api.models import  Category, Product
+from api.serializers import CategorySerializer, ProductSerializer, UserSerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+
+
+
+@api_view(['GET', 'POST'])
+def category_list(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET'])
+def products_by_category(request, category_id):
+    try:
+        categories = Category.objects.get(id=category_id)
+    except Category.DoesNotExist as e:
+        return Response({'error': str(e)})
+
+    if request.method == 'GET':
+        products = categories.products.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET', 'POST'])
+def products_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({'error': serializer.errors}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, product_id):
+    try:
+        products = Product.objects.get(id=product_id)
+    except Product.DoesNotExist as e:
+        return Response({'error': str(e)})
+    if request.method == 'GET':
+        serializer = ProductSerializer(products)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(instance=products, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response({'error': serializer.errors})
+
+    elif request.method == 'DELETE':
+        products.delete()
+        return Response({'deleted': True})
+
+
+
+class UserAPIView(APIView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data)
+
+
+
+class UserDetailsAPIView(APIView):
+        def get_object(self, id):
+            try:
+                return User.objects.get(id=id)
+            except User.DoesNotExist as e:
+                return Response({'error': str(e)})
+
+        def get(self, request, id):
+            user = self.get_object(id)
+            serializer = UserSerializer(user)
+            return Response(serializer.data)
+
